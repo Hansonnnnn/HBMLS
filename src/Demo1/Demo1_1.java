2560bee6205663b2136d7dfbfac41b6150333d7f
alex2097 <yym19960930@163.com>
2016-11-05 20:41:53 +0800

    first commit

---
+++ src/Demo1/Demo1_1.java
@@ -0,0 +1,12 @@
+package Demo1;
+
+/**
+ * Created by alex on 16-11-5.
+ */
+public class Demo1_1 {
+    private int d;
+    public Demo1_1(int n){
+        d=n;
+
+    }
+}
---
+++ src/Demo1/Demo1_2.java
@@ -0,0 +1,11 @@
+package Demo1;
+
+/**
+ * Created by alex on 16-11-5.
+ */
+public class Demo1_2 {
+    private int dd;
+    public Demo1_2(int n){
+        dd=n;
+    }
+}
